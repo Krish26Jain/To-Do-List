4c28482716cf56ecb04b8f1c95ec96973e45acdc
Krish26Jain <krishjain2609@gmail.com>
2024-07-18 09:13:37 +0530

    date button added and working

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let todoList = ['buy milk','go to collage'];
+let todoList = [
+  {
+    item:'buy milk',
+    duedate:'18/06/2005',
+  },
+  {
+    item:'go to collage',
+    duedate:'14/10/2008',
+  },
+];
 displayItems();
 
 function addTodo() {
   let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
   let todoItems = inputElement.value;
+  let todoDate = dateElement.value;
   todoList.push(todoItems);
+  todoList.push(todoDate);
   inputElement.value = '';
+  dateElement.value = '';
 
   displayItems();
 }
@@ -16,10 +29,13 @@ function displayItems() {
 
 
   for (let i = 0; i< todoList.length; i++) {
+    // let item =  todoList[i].item;
+    // let duedate = todoList[i].duedate;
+    let{item, duedate} = todoList[i];
     newHtml += 
-
        `<div>
-       <span>${todoList[i]}</span>
+       <span>${item}</span>
+       <span>${duedate}</span>
         <button onclick="todoList.splice(${i},1);displayItems();">Delete</button>
         </div>
       `;
